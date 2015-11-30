16ededaa215779253ba1b527cc6b3b0ec33141bb
Jake Wood <Jake.Wood@akqa.com>
2015-11-30 13:45:14 -0800

    Turned - to _ in table title

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM story-database', function(err, result) {
+    client.query('SELECT * FROM story_database', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
