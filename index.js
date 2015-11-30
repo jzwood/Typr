189ff82f3ecf7f1125b627ff304446b4ad6dcf53
Jake Wood <Jake.Wood@akqa.com>
2015-11-30 15:02:45 -0800

    test

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -8,6 +9,18 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+// app.get('/', function(request, response) {
+//   var result = ''
+//   var times = process.env.TIMES || 5
+//   for (i=0; i < times; i++)
+//     result += cool();
+//   response.send(result);
+// });
 
 var pg = require('pg');
 
